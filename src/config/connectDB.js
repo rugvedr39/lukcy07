22565e1d7e252a1cf7796d1950004a2a8fb60db7
Rugved-Technify <rugvedr39@gmail.com>
2024-10-28 14:32:48 +0530

    firt commit

---
+++ src/config/connectDB.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'database-1.c1eua666qm6x.eu-north-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Rugved8600988002',
+    database: 'bdg',
+});
+
+export default connection;
